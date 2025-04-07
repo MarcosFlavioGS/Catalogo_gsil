4ff4f1c6d358df90632acb9d9adf10136081ae5a
Marcos Flavio <mfghost69@gmail.com>
2025-04-07 13:50:26 -0300

    feat: change logo display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,19 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Catalogo',
-  description: 'Catalogo de produtos GSil Papeis'
+  description: 'Catalogo de produtos GSil Papeis',
+  icons: {
+    icon: '/logo.svg',
+    shortcut: '/logo.svg',
+    apple: '/logo.svg',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/logo.svg'
+    }
+  }
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
