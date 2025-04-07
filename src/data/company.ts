c3645d4f3ec70eefd5893784ecfb43066f4b27d9
Marcos Flavio <mfghost69@gmail.com>
2025-04-07 16:49:45 -0300

    change the company name

--- src/data/company.ts
+++ src/data/company.ts
@@ -4,7 +4,7 @@ export const company: Company = {
   name: 'GSil Papéis',
   webSite: 'https://www.gsilpapeis.com',
   logoUrl: '/logo.jpg',
-  phone: '11981634571',
+  phone: '1124513366',
   about:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
 }
